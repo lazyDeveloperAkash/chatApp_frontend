cc20738cbd5253baf0fa4b82ac749534264000d9
lazyDeveloperAkash <lazydeveloperakash@gmail.com>
2024-08-13 21:42:47 +0530

    axios api change

--- middleware.js
+++ middleware.js
@@ -4,13 +4,13 @@ export function middleware(request) {
     const token = request.cookies.get("token")?.value;
     console.log(token)
     console.log(request.nextUrl.pathname)
-    if (request.nextUrl.pathname == "/") {
-        console.log("first")
-        if (token) return NextResponse.redirect(new URL("/auth", request.url));
-    }
-    if(request.nextUrl.pathname == "/auth") {
-        if (!token) return NextResponse.redirect(new URL("/", request.url));
-    }
+    // if (request.nextUrl.pathname == "/") {
+    //     console.log("first")
+    //     if (token) return NextResponse.redirect(new URL("/auth", request.url));
+    // }
+    // if(request.nextUrl.pathname == "/auth") {
+    //     if (!token) return NextResponse.redirect(new URL("/", request.url));
+    // }
 }
 
 export const config = {
